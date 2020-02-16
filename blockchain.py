480365324252b0923e9c71c238291d86279caa90
micool91@gmail.com <micool91@gmail.com>
2020-02-16 15:30:48 +0100

    basic wallet implemented

--- blockchain.py
+++ blockchain.py
@@ -1,38 +1,38 @@
 from functools import reduce
-import hashlib
-from collections import OrderedDict
+import hashlib as hl
+
 import json
 import pickle
 
 from utility.hash_util import hash_block
-from utility.veryfication import Veryfication
+from utility.verification import Verification
 from block import Block
 from transaction import Transaction
+from wallet import Wallet
 
 MINING_REWARD = 10
+
+print(__name__)
 
 class Blockchain:
     def __init__(self, hosting_node_id):
         genesis_block = Block(0, '', [], 100, 0)
         self.chain = [genesis_block]
         self.__open_transactions = []
-        self.load_data
+        self.load_data()
         self.hosting_node = hosting_node_id
 
-    
     @property
     def chain(self):
         return self.__chain[:]
 
-    
-    @chain.setter
+    @chain.setter 
     def chain(self, val):
         self.__chain = val
 
 
-    def get_open_transactons(self):
+    def get_open_transactions(self):
         return self.__open_transactions[:]
-
 
     def load_data(self):
         try:
@@ -42,16 +42,21 @@ class Blockchain:
                 # blockchain = file_content['chain']
                 # open_transactions = file_content['ot']
                 blockchain = json.loads(file_content[0][:-1])
+                # We need to convert  the loaded data because Transactions should use OrderedDict
                 updated_blockchain = []
                 for block in blockchain:
-                    converted_tx = [Transaction(tx['sender'], tx['recipient'], tx['amount']) for tx in block['transactions']]
-                    updated_block = Block(block['index'], block['previous_hash'], converted_tx, block['proof'], block['timestamp'])
+                    converted_tx = [Transaction(
+                        tx['sender'], tx['recipient'], tx['signature'], tx['amount']) for tx in block['transactions']]
+                    updated_block = Block(
+                        block['index'], block['previous_hash'], converted_tx, block['proof'], block['timestamp'])
                     updated_blockchain.append(updated_block)
                 self.chain = updated_blockchain
                 open_transactions = json.loads(file_content[1])
+                # We need to convert  the loaded data because Transactions should use OrderedDict
                 updated_transactions = []
                 for tx in open_transactions:
-                    updated_transaction = Transaction(tx['sender'], tx['recipient'], tx['amount'])
+                    updated_transaction = Transaction(
+                        tx['sender'], tx['recipient'], tx['signature'], tx['amount'])
                     updated_transactions.append(updated_transaction)
                 self.__open_transactions = updated_transactions
         except (IOError, IndexError):
@@ -59,11 +64,11 @@ class Blockchain:
         finally:
             print('Cleanup!')
 
-
     def save_data(self):
         try:
             with open('blockchain.txt', mode='w') as f:
-                saveable_chain = [block.__dict__ for block in [Block(block_el.index, block_el.previous_hash, [tx.__dict__ for tx in block_el.transactions], block_el.proof, block_el.timestamp) for block_el in self.__chain]]
+                saveable_chain = [block.__dict__ for block in [Block(block_el.index, block_el.previous_hash, [
+                    tx.__dict__ for tx in block_el.transactions], block_el.proof, block_el.timestamp) for block_el in self.__chain]]
                 f.write(json.dumps(saveable_chain))
                 f.write('\n')
                 saveable_tx = [tx.__dict__ for tx in self.__open_transactions]
@@ -76,66 +81,54 @@ class Blockchain:
         except IOError:
             print('Saving failed!')
 
-
     def proof_of_work(self):
         last_block = self.__chain[-1]
         last_hash = hash_block(last_block)
         proof = 0
-        while not Veryfication.valid_proof(self.__open_transactions, last_hash, proof):
+        # Try different PoW numbers and return the first valid one
+        while not Verification.valid_proof(self.__open_transactions, last_hash, proof):
             proof += 1
         return proof
-
 
     def get_balance(self):
         participant = self.hosting_node
         tx_sender = [[tx.amount for tx in block.transactions
-                    if tx.sender == participant] for block in self.__chain]
+                      if tx.sender == participant] for block in self.__chain]
         open_tx_sender = [tx.amount
-                        for tx in self.__open_transactions if tx.sender == participant]
+                          for tx in self.__open_transactions if tx.sender == participant]
         tx_sender.append(open_tx_sender)
+        print(tx_sender)
         amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
-                            if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+                             if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
         tx_recipient = [[tx.amount for tx in block.transactions
-                        if tx.recipient == participant] for block in self.__chain]
-        amount_recieved = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
-                                if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
-        return amount_recieved - amount_sent
-
+                         if tx.recipient == participant] for block in self.__chain]
+        amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
+                                 if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+        return amount_received - amount_sent
 
     def get_last_blockchain_value(self):
-        """ Returns the last value of the current blockchain. """
         if len(self.__chain) < 1:
             return None
         return self.__chain[-1]
 
-
-    # This function accepts two arguments.
-    # One required one (transaction_amount) and one optional one (last_transaction)
-    # The optional one is optional because it has a default value => [1]
-
-
-    def add_transaction(self, recipient, sender, amount=1.0):
-        """ Append a new value as well as the last blockchain value to the blockchain.
-
-        Arguments:
-            :sender: 
-            :recipient: 
-            :amount: 
-        """
+    def add_transaction(self, recipient, sender, signature, amount=1.0):
         # transaction = {
         #     'sender': sender,
         #     'recipient': recipient,
         #     'amount': amount
         # }
-        transaction = Transaction(sender, recipient, amount)
-        if Veryfication.verify_transaction(transaction, self.get_balance):
+        if self.hosting_node == None:
+            return False
+        transaction = Transaction(sender, recipient, signature, amount)
+        if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
             return True
         return False
 
-
     def mine_block(self):
+        if self.hosting_node == None:
+            return False
         last_block = self.__chain[-1]
         hashed_block = hash_block(last_block)
         proof = self.proof_of_work()
@@ -144,10 +137,14 @@ class Blockchain:
         #     'recipient': owner,
         #     'amount': MINING_REWARD
         # }
-        reward_transaction = Transaction('MINING', self.hosting_node, MINING_REWARD)
+        reward_transaction = Transaction('MINING', self.hosting_node, '', MINING_REWARD)
         copied_transactions = self.__open_transactions[:]
+        for tx in copied_transactions:
+            if not Wallet.verify_transaction(tx):
+                return False
         copied_transactions.append(reward_transaction)
-        block = Block(len(self.__chain), hashed_block, copied_transactions, proof)
+        block = Block(len(self.__chain), hashed_block,
+                      copied_transactions, proof)
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
--- utility/verification.py
+++ utility/verification.py
@@ -1,34 +1,35 @@
 """Provides verification helper methods."""
 
-
 from utility.hash_util import hash_string_256, hash_block
+from wallet import Wallet
 
-class Veryfication:
+class Verification:
     @staticmethod
     def valid_proof(transactions, last_hash, proof):
-        guess = (str(tx.to_ordered_dict() for tx in transactions) + str(last_hash) + str(proof)).encode()
+        guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
         guess_hash = hash_string_256(guess)
-        print(guess_hash)
         return guess_hash[0:2] == '00'
-
+        
     @classmethod
-    def veryfy_chain(cls, blockchain):
-        """" verify chain """
+    def verify_chain(cls, blockchain):
         for (index, block) in enumerate(blockchain):
             if index == 0:
                 continue
             if block.previous_hash != hash_block(blockchain[index - 1]):
                 return False
             if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-                print('Proof of work is invalid!')
+                print('Proof of work is invalid')
                 return False
         return True
 
     @staticmethod
-    def verify_transaction(transaction, get_balance):
-        sender_balance = get_balance()
-        return sender_balance >= transaction.amount
+    def verify_transaction(transaction, get_balance, check_funds=True):
+        if check_funds:
+            sender_balance = get_balance()
+            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+        else:
+            return Wallet.verify_transaction(transaction)
 
     @classmethod
     def verify_transactions(cls, open_transactions, get_balance):
-        return all([cls.verify_transaction(tx, get_balance) for tx in open_transactions])
+        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transactions])
